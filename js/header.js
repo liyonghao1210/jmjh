f84bc847aebc21b23bf48007694415cad8c7a38e
liyonghao1210 <lyh_md@163.com>
2018-05-28 15:48:14 +0800

    下午第一次

---
+++ js/header.js
@@ -0,0 +1,42 @@
+//兼容火狐、IE8   
+//显示遮罩层    
+function showMask() {
+    $("#mask").css("height", $(document).height());
+    $("#mask").css("width", $(document).width());
+    $("#mask").show();
+}
+//隐藏遮罩层  
+function hideMask() {
+    $("#mask").hide();
+    $(".login").hide();
+    $(".register").hide();
+}
+// 显示登录
+$(".dl-padding").click(function () {
+    $(".login").show();
+    showMask();
+})
+// 显示注册
+$(".zc-padding").click(function () {
+    $(".register").show();
+    showMask();
+})
+
+// 获取验证码
+var countdown = 60;
+
+function settime(obj) {
+    if (countdown == 0) {
+        obj.removeAttribute("disabled");
+        obj.value = "免费获取验证码";
+        countdown = 60;
+        return;
+    } else {
+        obj.setAttribute("disabled", true);
+        obj.value = "重新发送(" + countdown + ")";
+        countdown--;
+    }
+    setTimeout(function () {
+        settime(obj)
+    }, 1000)
+}
